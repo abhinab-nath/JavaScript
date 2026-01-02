2c3b431b737a13230831aa09ed0deba2a9856ce0
Abhinab Nath <abhinabnath105@gmail.com>
2026-01-02 11:09:58 +0530

    maths

---
+++ numbers_and_maths.js
@@ -0,0 +1,33 @@
+score = 400
+
+const balance = new Number(100)
+console.log(balance);   // [Number: 100]
+
+console.log(balance.toString().length); //3
+
+//toFixed
+console.log(balance.toFixed(2)); // 100.00 ; toFixed(1) then 100.0
+
+
+//toPrecision
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3)); // 124
+// if toPrecision(4) then 123.9 ; otherNumber = 1123.8966,and precision value is 3, then = 1.12e+3
+
+
+//toLocaleString
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); // 10,00,000
+console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+
+
+// MAX / MIN value, safe int value
+
+let max_num = Number.MAX_VALUE;  // 1.7976931348623157e+308
+let min_num = Number.MIN_VALUE;  // 5e-324                 
+
+let max_safe = Number.MAX_SAFE_INTEGER; // 9007199254740991
+let min_safe = Number.MIN_SAFE_INTEGER; // -9007199254740991 
+
+console.table(['max/min value:',max_num, min_num ,'safe int:', max_safe, min_safe]);
+
